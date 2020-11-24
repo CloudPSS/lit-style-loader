f149a1f9f6a5ad40bf2ba0e3aac4191c135a3c49
Opportunity <opportunity@live.in>
2020-11-24 10:24:44 +0800

    feat: supports hmr by contructed stylesheets

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const loaderUtils = require('loader-utils');
+const path = require("path");
+const loaderUtils = require("loader-utils");
 
 const loaderApi = () => {};
 
 loaderApi.pitch = function loader(request) {
+  const lib = loaderUtils.stringifyRequest(
+    this,
+    path.resolve(__dirname, "stylesToCssArray.js")
+  );
+  const source = loaderUtils.stringifyRequest(this, `!!${request}`);
+  if (this.hot) {
     return `
-import api from ${loaderUtils.stringifyRequest(this, path.resolve(__dirname, 'stylesToCssArray.js'))}
-import content from ${loaderUtils.stringifyRequest(this, `!!${request}`)};
-export default api(content);`;
+import { load, update } from ${lib};
+import content from ${source};
+
+let result = load(content);
+if (module.hot) {
+    module.hot.accept(${source}, function () {
+        const updated = update(result, content);
+        if (!updated) throw new Error("Can't hmr");
+    });
+}
+export default result;`;
+  } else {
+    return `
+import { load } from ${lib};
+import content from ${source};
+export default load(content);`;
+  }
 };
 
 module.exports = loaderApi;
--- stylesToCssArray.js
+++ stylesToCssArray.js
@@ -1,21 +1,58 @@
-'use strict';
+"use strict";
 
-const { unsafeCSS } = require('lit-element');
+import { unsafeCSS, CSSResult, supportsAdoptingStyleSheets } from "lit-element";
 
+/**
+ * @param {[string, string, string, string]} item
+ * @returns {CSSResult}
+ */
 function toCssResult(item) {
-    let [id, css, media, sourceMap] = item || [];
-    if (media) {
-        throw new Error(`Css with media query is not supported, ${id}`);
-    }
-    if (sourceMap) {
-        css = `${css}
-/*# sourceMappingURL=data:application/json;base64,${btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))))} */
+  let [id, css, media, sourceMap] = item || [];
+  if (media) {
+    throw new Error(`Css with media query is not supported, ${id}`);
+  }
+  if (sourceMap) {
+    css = `${css}
+/*# sourceMappingURL=data:application/json;base64,${btoa(
+      unescape(encodeURIComponent(JSON.stringify(sourceMap)))
+    )} */
 `;
-    }
-    return unsafeCSS(css);
+  }
+  const result = unsafeCSS(css);
+  result.id = id;
+  return result;
 }
 
-module.exports = function (list) {
-    list = list || [];
-    return list.map((i) => toCssResult(i));
-};
+/**
+ * @param {[string, string, string, string][]} list
+ * @returns {CSSResult[]}
+ */
+export function load(list) {
+  list = list || [];
+  return list.map((i) => toCssResult(i));
+}
+
+/**
+ * @param {CSSResult[]} css
+ * @param {[string, string, string, string|undefined][]} list
+ *
+ * @returns {boolean}
+ */
+export function update(css, list) {
+  list = list || [];
+  if (css.length !== list.length) return false;
+  if (css.length !== list.length) return false;
+  if (!supportsAdoptingStyleSheets) return false;
+  if (list.some(([id]) => !css.find((c) => c.id === id))) return false;
+  if (css.some(({ id }) => !list.find(([lid]) => id === lid)))
+    return load(list);
+  const newList = load(list);
+  for (const result of css) {
+    const newItem = newList.find((c) => c.id === result.id);
+    result.cssText = newItem.cssText;
+    if (result._styleSheet && supportsAdoptingStyleSheets) {
+      result._styleSheet.replaceSync(newItem.cssText);
+    }
+  }
+  return true;
+}
